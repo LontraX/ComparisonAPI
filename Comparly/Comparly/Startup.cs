bff3d293b324574918d9bae0c3cd6ea68ef79b2e
LontraX <73993263+LontraX@users.noreply.github.com>
2022-01-11 18:44:48 +0100

    added jwt security schema to swaggerGen

--- Comparly/Comparly/Startup.cs
+++ Comparly/Comparly/Startup.cs
@@ -88,6 +88,25 @@ namespace Comparly
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Comparly", Version = "v1" });
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    { securitySchema, new[] { "Bearer" } }
+                };
+                c.AddSecurityRequirement(securityRequirement);
             });
         }
 
