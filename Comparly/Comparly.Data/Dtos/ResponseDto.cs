0212b4a868c38f0968d5b1a5be51c25331b575b8
LontraX <73993263+LontraX@users.noreply.github.com>
2022-01-11 14:17:32 +0100

    updated the compare controller

---
+++ Comparly/Comparly.Data/Dtos/ResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparly.Data.Dtos
+{
+    public class ResponseDto<T>
+    {
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
