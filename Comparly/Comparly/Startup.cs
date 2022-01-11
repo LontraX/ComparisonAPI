fbc4c0b7fbc18d309161b49d424d747191f18bff
LontraX <73993263+LontraX@users.noreply.github.com>
2022-01-11 19:33:52 +0100

    startup changes

--- Comparly/Comparly/Startup.cs
+++ Comparly/Comparly/Startup.cs
@@ -113,7 +113,7 @@ namespace Comparly
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() && env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
